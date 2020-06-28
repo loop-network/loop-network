62b4508d7614ba592a1430b6e56179090a82347c
scye09 <scye09@gmail.com>
2020-06-27 23:17:56 -0400

    fix image sizing community page

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -22,34 +22,81 @@ function Community() {
           </div>
         </div>
       </div>
-      {/* <div class="row min-vh-100 py-4 px-4 my-4 mt-0"></div> */}
+      <div class="row min-vh-100 py-4 px-4 my-4">
         <div class="container my-auto">
-          <div class="card-group">
-            <div class="card">
-              <img src="images/slack_chat.svg" class="card-img-top" alt="One on Ones Card" />
-              <div class="card-body">
-                <h5 class="card-title">Our Slack Workspace</h5>
-                <p class="card-text">Join the Slack community to be a part of the conversation</p>
-                <a href="/Matching" class="btn btn-cta">Join Now</a>
+          <div class="card-deck">
+            <div class="col-md-6 col-lg-6 col-12 align-items-stretch mb-4">
+              <div class="card mb-4 h-100">
+                <img src="images/slack_chat.svg" class="card-img-top larger-card" alt="One on Ones Card" />
+                <div class="card-body">
+                  <h5 class="card-title">Our Slack Workspace</h5>
+                  <p class="card-text">Join the Slack community to be a part of the conversation.</p>
+                  <a href="/Matching" class="btn btn-cta">Join Now</a>
+                </div>
               </div>
             </div>
-            <div class="card">
-              <img src="images/women_directory.svg" class="card-img-top" alt="Group Salons Card" />
-              <div class="card-body">
-                <h5 class="card-title">Our Directory of Womxn</h5>
-                <p class="card-text">Access the Directory to reach out directly to other interesting womxn.</p>
-                <a href="/Groups" class="btn btn-cta">Go to Directory</a>
+            <div class="col-md-6 col-lg-6 col-12 align-items-stretch mb-4">
+              <div class="card mb-4 h-100">
+                <img src="images/women_directory.svg" class="card-img-top larger-card" alt="Group Salons Card" />
+                <div class="card-body">
+                  <h5 class="card-title">Our Directory of Womxn</h5>
+                  <p class="card-text">Access the Directory to reach out directly to other interesting womxn.</p>
+                  <a href="/Groups" class="btn btn-cta">Go to Directory</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      
-      <div class="row min-vh-100 blue-background py-4 px-4 my-4">
+      </div>
+      <div class="row min-vh-100 py-4 px-4 my-4">
         <div class="container my-auto">
-          <h1>“I met one of my technical co-founders of my startup through a one-on-one set up by The Loop!” - Jan Smith</h1>
+          <h5></h5>
+          <div class="card-deck">
+            <div class="col-md-6 col-lg-6 col-12 d-flex align-items-stretch">
+                <div class="card mb-4">
+                <div class="card-body yellow-background">
+                  <p class="card-text">"I absolutely love this platform and engage with it every day! Changed my life. Here are more words of praise and this is a long testimonial. I love The Loop Network and I recommend all women founders join!"</p>
+                </div>
+                <div class="card-body">
+                  <div class="row speaker">
+                    <div class="col-md-3 col-lg-3 col-3 d-flex align-items-stretch">
+                      <div class="avatar mx-auto">
+                        <img src="images/example_headshot.jpg" class="rounded-circle z-depth-1 img-fluid" />
+                      </div>
+                    </div>
+                    <div class="col-md-9 col-lg-9 col-9">
+                      <p class="card-title"><b>Melinda Gates</b></p>
+                      <p class="card-subtitle mb-2 text-muted">Co-founder of the Gates Foundation</p>
+                    </div>
+                </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-12 d-flex align-items-stretch">
+              <div class="card mb-4">
+                <div class="card-body yellow-background">
+                  <p class="card-text">"I absolutely love this platform and engage with it every day! Changed my life. This is a shorter testimonial."</p>
+                </div>
+                <div class="card-body">
+                  <div class="row speaker">
+                    <div class="col-md-3 col-lg-3 col-3 d-flex align-items-stretch">
+                      <div class="avatar mx-auto">
+                        <img src="images/example_headshot.jpg" class="rounded-circle z-depth-1 img-fluid" />
+                      </div>
+                    </div>
+                    <div class="col-md-9 col-lg-9 col-9">
+                      <p class="card-title"><b>Melinda Gates</b></p>
+                      <p class="card-subtitle mb-2 text-muted">Co-founder of the Gates Foundation</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
   );
 }
 
