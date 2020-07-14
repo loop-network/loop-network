f4f8e5bf21984aa9818fcf4241ae021b73b33321
scye09 <scye09@gmail.com>
2020-07-14 19:36:04 -0400

    add mailchimp signup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,26 @@ function Footer() {
     <div className="footer">
       <footer class="py-5 bg-white px-4 my-4">
         <div class="container">
-          <h2>Stay in the Loop</h2>
+          <h2 class="mb-2">Stay in the Loop</h2>
           <div class="row">
             <div class="col-lg-6 mb-4">
-              <h5>Receive content, events, and more.</h5>
-                <form>
-                  <div class="form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address"/>
-                  </div>
-                  <button type="submit" class="btn btn-primary">Subscribe</button>
-                </form>
-            </div>
+              <h5 class="mb-2">Receive content, events, and more.</h5>
+                <div id="mc_embed_signup">
+                  <form action="https://squarespace.us19.list-manage.com/subscribe/post?u=192e08f3316678f1d6916d453&amp;id=f7ea5c925e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                      <input type="email" name="EMAIL" class="form-control mb-2" id="mce-EMAIL" placeholder="Email Address" aria-describedby="emailHelp" required />
+                      <div id="some-box" aria-hidden="true">
+                        <input type="text" name="b_192e08f3316678f1d6916d453_f7ea5c925e" tabindex="-1" />
+                      </div>
+                      <div class="clear">
+                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary"/>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             <div class="col-lg-6">
-              <h5>Contact us and stay updated.</h5>
+              <h5 class="mb-2">Contact us and stay updated.</h5>
               <p><FontAwesomeIcon icon={faEnvelopeSquare} /> helloloopnetwork@gmail.com</p>
               <p><FontAwesomeIcon icon={faLinkedin} /> @helloloopnetwork</p>
               <p><FontAwesomeIcon icon={faTwitter} /> @helloloopnetwork</p>
