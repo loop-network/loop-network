6f4d2964558108408e406abb90028b2649a5029b
Annie Lu <al343@duke.edu>
2020-07-14 17:18:22 -0700

    Updates

---
+++ precache-manifest.75ab39f22d646401e54f7f9b92886b9d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31a3cb5fdc8eb6b96dde193c8f70c71",
+    "url": "/loop-network/index.html"
+  },
+  {
+    "revision": "164e703739397926fc4b",
+    "url": "/loop-network/static/css/main.e6b0cfd8.chunk.css"
+  },
+  {
+    "revision": "1991cfc97cdfafd12b88",
+    "url": "/loop-network/static/js/2.d338abab.chunk.js"
+  },
+  {
+    "revision": "8a666dd660e108ab4f1e0f22fb2e9cd2",
+    "url": "/loop-network/static/js/2.d338abab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164e703739397926fc4b",
+    "url": "/loop-network/static/js/main.56591a62.chunk.js"
+  },
+  {
+    "revision": "8bf067e01eacd0df09bc",
+    "url": "/loop-network/static/js/runtime-main.dabb34ba.js"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/loop-network/static/media/GlacialIndifference-Bold.f801e234.otf"
+  },
+  {
+    "revision": "69b161f1f0d94f15afefbd34a37566d8",
+    "url": "/loop-network/static/media/GlacialIndifference-Italic.69b161f1.otf"
+  },
+  {
+    "revision": "13dc531a2ffd9ca7eeb500515774fa74",
+    "url": "/loop-network/static/media/GlacialIndifference-Regular.13dc531a.otf"
+  },
+  {
+    "revision": "33c1c2f2ada08cab52a7f42177ea44ca",
+    "url": "/loop-network/static/media/Now-Bold.33c1c2f2.otf"
+  },
+  {
+    "revision": "15620817bd6eb0b95c91ee1510eb557c",
+    "url": "/loop-network/static/media/glacialindifference-bold-webfont.15620817.svg"
+  },
+  {
+    "revision": "886ae167e4fcd70bf5e83353192bd831",
+    "url": "/loop-network/static/media/glacialindifference-bold-webfont.886ae167.woff"
+  },
+  {
+    "revision": "bddbbf76607334f4f20e0c6880995ce2",
+    "url": "/loop-network/static/media/glacialindifference-bold-webfont.bddbbf76.eot"
+  },
+  {
+    "revision": "16b6f2ca9fe140122b806713b43c1d64",
+    "url": "/loop-network/static/media/glacialindifference-italic-webfont.16b6f2ca.woff"
+  },
+  {
+    "revision": "2c47cd7c9df784e78153f6711553a5d7",
+    "url": "/loop-network/static/media/glacialindifference-italic-webfont.2c47cd7c.svg"
+  },
+  {
+    "revision": "3f4439b813f531352267fb61e377967e",
+    "url": "/loop-network/static/media/glacialindifference-italic-webfont.3f4439b8.eot"
+  },
+  {
+    "revision": "0aad8d1cfe988b48cf2ca70c88415df9",
+    "url": "/loop-network/static/media/glacialindifference-regular-webfont.0aad8d1c.eot"
+  },
+  {
+    "revision": "244318e580ada394adccd3e4f6326727",
+    "url": "/loop-network/static/media/glacialindifference-regular-webfont.244318e5.woff"
+  },
+  {
+    "revision": "c6b8ace0accf4262d124fdd845a0f586",
+    "url": "/loop-network/static/media/glacialindifference-regular-webfont.c6b8ace0.svg"
+  },
+  {
+    "revision": "31eca0fc1d0c73cc0e80a6b5a772a882",
+    "url": "/loop-network/static/media/now-bold-webfont.31eca0fc.svg"
+  },
+  {
+    "revision": "945469b87d2610cc99070c119dd0e3d0",
+    "url": "/loop-network/static/media/now-bold-webfont.945469b8.woff"
+  },
+  {
+    "revision": "d8c209ae3be9443ac1433f19dfbd8d78",
+    "url": "/loop-network/static/media/now-bold-webfont.d8c209ae.eot"
+  }
+]);
