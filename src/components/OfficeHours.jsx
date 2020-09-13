65f6ce2c4e6770d480b2481f299d42b52e7c5aa7
scye09 <scye09@gmail.com>
2020-09-13 10:58:41 -0400

    add office hours signup

--- src/components/OfficeHours.jsx
+++ src/components/OfficeHours.jsx
@@ -12,6 +12,23 @@ function OfficeHours() {
           <iframe class="airtable-embed" src="https://airtable.com/embed/shrStuTLKWlqTQjSQ?backgroundColor=pink&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: "transparent", border: "1px solid #ccc"}}></iframe>
         </div>
       </div>
+      <div class="row min-vh-75 py-4 px-4 my-4">
+        <div class="container my-auto">
+          <div class="row align-items-center my-5">
+            <div class="col-lg-6">
+              <img
+                class="img-fluid rounded mb-4 mb-lg-0"
+                src="illustrations/calendar.svg"
+                alt="Teaching"
+              />
+            </div>
+            <div class="col-lg-6 mb-4">
+              <h2>Interested in becoming an Office Hours Mentor?</h2>
+              <a href="https://airtable.com/shrAlZeG7cxsDPumC"><button type="button" class="btn btn-primary">Sign up now</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   );
