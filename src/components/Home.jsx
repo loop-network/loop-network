6b6ca630dc809c5fc83ac4d67437641722831c99
scye09 <sophiaye@wharton.upenn.edu>
2021-03-12 23:10:10 -0500

    add animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,65 @@
 import React from "react";
 
 function Home() {
+  var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 200 - Math.random() * 100; // change this number to change speed of typing out the word
+
+    if (this.isDeleting) {
+      delta /= 2.5; // change this number to change speed of deletion
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    var elements = document.getElementsByClassName("txt-rotate");
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute("data-rotate");
+      var period = elements[i].getAttribute("data-period");
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML =
+      ".txt-rotate > .wrap { border-right: 0.08em solid rgba(255, 255, 255, .5); }";
+    document.body.appendChild(css);
+  };
   return (
     <div className="home">
       <div class="row min-vh-75 blue-background py-4 px-4 mb-4">
@@ -12,7 +71,12 @@ function Home() {
                 innovation.
               </p>
               <h1 class="mb-4">
-                It's from a disparity in access to resources.
+                It's from a disparity in <br />
+                <span
+                  class="txt-rotate"
+                  data-period="1500" // change this number to change length of time between each word
+                  data-rotate='[ "resources.", "access to opportunities.", "access to support.", "confidence." ]'
+                ></span>
               </h1>
             </div>
             <div class="col-lg-6">
